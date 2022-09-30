70c9f6b9aa3b025948fca8f60bdf037510080e14
Budderstock <shanejanish@gmail.com>
2022-09-30 09:50:56 -0500

    change 1

--- src/main/java/Methodologies.java
+++ src/main/java/Methodologies.java
@@ -1,16 +1,18 @@
-import java.util.Random;
+import java.ytil
 
 public class Methodologies {
 
     public static double howFar(int xOne, int yOne, int xTwo, int yTwo )
     {
-        return 0;
+        return (Math.sqrt((yTwo - yOne)*(yTwo - yOne)+(xTwo - xOne)*(xTwo-xOne)));
     }
 
-    public static double howFast( int distanceInMiles, int hours, int minutes )
-    {
-        return 0;
-    }
+   // public static double howFast( int distanceInMiles, int hours, int minutes )
+   // {
+   //     return (distanceInMiles/(hours*(minutes/60)));
+   // }
+
+
 
     public static double getRootAdd( int a, int b, int c )
     {
